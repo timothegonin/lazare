f199a078165911f1fffdef338ab245768eb12138
timotheG <gonin.timothe@gmail.com>
2024-12-10 17:20:54 +0100

    💄 adding footer styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,30 +4,41 @@ import FacebookIcon from '../assets/logo/social_links/facebook.svg'
 import InstagramIcon from '../assets/logo/social_links/instagram.svg'
 import TripadvisorIcon from '../assets/logo/social_links/tripadvisor.svg'
 
+const Divider = () => {
+  return (
+    <span className='bg-[#C7A99A] h-[0.2px] w-14 mt-1 mb-3'></span>
+  )
+}
 
 const Footer = () => {
   return (
-    <footer className="bg-[#F2F2F2]">
-      <div>
-        <h2>Adresse</h2>
-        <ul>
-          <li><GeoIcon/></li>
-          <li>5 rue Lazare Escarguel, 66000 PERPIGNAN</li>
+    <footer className="bg-[#F2F2F2] flex flex-col gap-5 py-10 px-10">
+      {/* 1 ADRESS */}
+      <div className='flex flex-col items-start'>
+        <h2 className='text-xl'>Adresse</h2>
+        <Divider/>
+        <ul className='list-none flex flex-row gap-4'>
+          <li className='m-auto'><GeoIcon fill='#C7A99A'/></li>
+          <li className='font-light'>5 rue Lazare Escarguel, 66000 Perpignan</li>
         </ul>
       </div>
-      <div>
-        <h2>Nous contacter</h2>
-        <ul>
-          <li><PhoneIcon/></li>
-          <li>04 68 21 60 42</li>
+      {/* 2 CONTACT */}
+      <div className='flex flex-col items-start'>
+        <h2 className='text-xl'>Nous contacter</h2>
+        <Divider/>
+        <ul className='list-none flex flex-row gap-4'>
+          <li className='m-auto'><PhoneIcon fill='#C7A99A'/></li>
+          <li className='font-light'>04 68 21 60 42</li>
         </ul>
       </div>
-      <div>
-        <h2>Nous suivre</h2>
-        <ul>
-          <li><FacebookIcon/></li>
-          <li><InstagramIcon/></li>
-          <li><TripadvisorIcon/></li>
+      {/* 3 FOLLOWING */}
+      <div className='flex flex-col items-start'>
+        <h2 className='text-xl'>Nous suivre</h2>
+        <Divider/>
+        <ul className='list-none flex flex-row gap-4'>
+          <li className='m-auto'><FacebookIcon fill='#C7A99A'/></li>
+          <li className='m-auto'><InstagramIcon fill='#C7A99A'/></li>
+          <li className='m-auto'><TripadvisorIcon fill='#C7A99A'/></li>
         </ul>
       </div>
     </footer>
