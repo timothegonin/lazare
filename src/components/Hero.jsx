dea1a2cdfa55083d74451ca1821497693d1c29cf
timotheG <gonin.timothe@gmail.com>
2024-12-16 17:44:53 +0100

    ✏️ improving Hero's citation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,10 +8,8 @@ const Hero = () => {
         <h1 className="hidden">Lazare</h1>
       </div>
       <div className="bg-[url('/src/assets/images/recipe.jpg')] bg-clip-border bg-cover bg-no-repeat bg-center h-[550px]">
-        <div className="pt-14 px-5 pb-24" style={{background: "linear-gradient(180deg, rgba(28,52,84,1) 44%, rgba(255,255,255,0) 100%)"}}>
-          <div className="bg-[url('/src/assets/images/citation.svg')] bg-clip-border bg-cover bg-no-repeat bg-center h-[24px]">
-            <h3 className="hidden">Pas de hasard, que des rendez-vous</h3>
-          </div>
+        <div className="pt-14 px-5 pb-32 h-[24px]" style={{background: "linear-gradient(180deg, rgba(28,52,84,1) 44%, rgba(255,255,255,0) 100%)"}}>
+            <h3 className="text-3xl text-[#C7A99A] citation ">Pas de hasard, que des rendez-vous</h3>
         </div>
         <button className="px-4 py-3 rounded-md bg-[#C7A99A] text-[#F2F2F2]">Découvrir le menu</button>
       </div>
