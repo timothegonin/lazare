dfaf4eee776630f8afc08a4db3a5955303f72a06
timotheG <gonin.timothe@gmail.com>
2024-12-25 16:58:07 +0100

    🚧💄basic PagesNavigation styles

---
+++ src/components/PagesNavigation.jsx
@@ -0,0 +1,12 @@
+const PagesNavigation = () => {
+  return (
+    <nav className="flex justify-evenly h-[115px] bg-[#1C3454]">
+      <button className="text-2xl font-bold text-white underline">
+        La carte
+      </button>
+      <button className="text-2xl font-bold text-[#C7A99A]">Les menus</button>
+    </nav>
+  )
+}
+
+export default PagesNavigation
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,38 +1,45 @@
+import PagesNavigation from '../components/PagesNavigation'
+
 const Menu = () => {
   return (
-    <section className="bg-[#F2F2F2] p-10 text-left flex flex-col gap-5">
-      <h1>La carte</h1>
-      <table className="w-full">
-        <th>
-          <h2>ENTRÉES</h2>
-        </th>
-        <tr>
-          <td>
-            Maquereaux, arc en ciel de choux, vinaigrette estragon et miel de
-            Tautavel
-          </td>
-          <td className="align-top">17€</td>
-        </tr>
-      </table>
-      <table className="w-full">
-        <th>
-          <h2>PLATS</h2>
-        </th>
-        <tr>
-          <td>Dos de lieu jaune sauvage au savagnin, salsifis et trompettes</td>
-          <td className="align-top">35€</td>
-        </tr>
-      </table>
-      <table className="w-full">
-        <th>
-          <h2>DESSERTS</h2>
-        </th>
-        <tr>
-          <td>Crème brulée chou fleur, vanille bleue</td>
-          <td className="align-top">15€</td>
-        </tr>
-      </table>
-    </section>
+    <>
+      <PagesNavigation />
+      <section className="bg-[#F2F2F2] p-10 text-left flex flex-col gap-5">
+        <h1>La carte</h1>
+        <table className="w-full">
+          <th>
+            <h2>ENTRÉES</h2>
+          </th>
+          <tr>
+            <td>
+              Maquereaux, arc en ciel de choux, vinaigrette estragon et miel de
+              Tautavel
+            </td>
+            <td className="align-top">17€</td>
+          </tr>
+        </table>
+        <table className="w-full">
+          <th>
+            <h2>PLATS</h2>
+          </th>
+          <tr>
+            <td>
+              Dos de lieu jaune sauvage au savagnin, salsifis et trompettes
+            </td>
+            <td className="align-top">35€</td>
+          </tr>
+        </table>
+        <table className="w-full">
+          <th>
+            <h2>DESSERTS</h2>
+          </th>
+          <tr>
+            <td>Crème brulée chou fleur, vanille bleue</td>
+            <td className="align-top">15€</td>
+          </tr>
+        </table>
+      </section>
+    </>
   )
 }
 
