46aec9add39d7c56c3e496c8243de4bd25cc3bbd
timotheG <gonin.timothe@gmail.com>
2025-01-01 17:48:49 +0100

    📱 🚧 💄 header main navlinks

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types'
+import { NavLink } from 'react-router'
 import CallTo from './CallTo'
 import Logo from '../assets/logo/logo-min.svg'
 import PhoneIcon from '../assets/logo/social_links/phone.svg'
@@ -10,7 +11,42 @@ const Header = ({ statusHandler }) => {
       <div>
         <Logo width="120" />
       </div>
-      <nav className="flex justify-center gap-4">
+      <nav className="hidden items-center gap-4 min-[550px]:flex">
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive
+              ? 'text-white underline underline-offset-2'
+              : 'text-[#C7A99A]'
+          }
+        >
+          Accueil
+        </NavLink>
+        <NavLink
+          to="/menu"
+          className={({ isActive }) =>
+            isActive
+              ? 'text-white underline underline-offset-2'
+              : 'text-[#C7A99A]'
+          }
+        >
+          Découvrir la carte
+        </NavLink>
+        <NavLink
+          to="/formules"
+          className={({ isActive }) =>
+            isActive
+              ? 'text-white underline underline-offset-2'
+              : 'text-[#C7A99A]'
+          }
+        >
+          Découvrir les menus
+        </NavLink>
+        <CallTo phone="+33468216042">
+          <PhoneIcon fill="#C7A99A" width="16" height="16" />
+        </CallTo>
+      </nav>
+      <nav className="flex justify-center gap-4 min-[550px]:hidden">
         <div className="flex items-center">
           <CallTo phone="+33468216042">
             <PhoneIcon fill="#C7A99A" width="16" height="16" />
