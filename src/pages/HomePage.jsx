23495f68d059b87d6aaa971c16a158e37e21462c
timotheG <gonin.timothe@gmail.com>
2025-01-15 09:56:13 +0100

    ♻️ Adding Overlay component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,36 +1,40 @@
 import { Link } from 'react-router'
 import chefPicture from '../assets/images/lazareChef.jpg'
 
+const BlueOvelray = ({ position, children }) => {
+  return (
+    <div
+      className={`flex flex-col items-center absolute ${position} w-full`}
+      style={{
+        background:
+          'linear-gradient(180deg, rgba(28,52,84,1) 44%, rgba(255,255,255,0) 100%)',
+      }}
+    >
+      {children}
+    </div>
+  )
+}
+
 const HomePage = () => {
   return (
-    // <div className="bg-[#F2F2F2] bg-[url('/src/assets/images/lazareChef.jpg')] bg-clip-border bg-cover bg-no-repeat bg-center h-[550px]">
     <div className="relative flex flex-col">
-      <div className="flex flex-col items-center absolute left-0 right-0">
-        <div
-          className="pt-14 px-5 pb-32 h-[24px] w-full"
-          style={{
-            background:
-              'linear-gradient(180deg, rgba(28,52,84,1) 44%, rgba(255,255,255,0) 100%)',
-          }}
-        >
-          <h3 className="text-3xl text-[#C7A99A] citation ">
-            Pas de hasard, que des rendez-vous
-          </h3>
-        </div>
+      <BlueOvelray postition="left-0 right-0">
+        <h3 className="py-14 px-5 text-3xl text-[#C7A99A] citation ">
+          Pas de hasard, que des rendez-vous
+        </h3>
         <Link
           to="/menu"
           className="px-4 py-3 rounded-md bg-[#C7A99A] text-[#F2F2F2]"
         >
           Découvrir la carte & les menus
         </Link>
-      </div>
-      {/* <div className="bg-[#F2F2F2] bg-[url('/src/assets/images/lazareChef.jpg')] bg-clip-border bg-cover bg-no-repeat bg-[center_bottom_-5rem] h-[498px]"></div> */}
+      </BlueOvelray>
       <img
         className="object-cover "
         src={chefPicture}
         alt="Photograph of the chef at Lazare in front of his restaurant"
       />
-      <div className="bg-white text-start p-10 flex flex-col gap-5">
+      <div className="bg-white text-start p-10 flex flex-col gap-5 relative">
         <h3 className="text-center  chef-name text-xl">
           Charles : Le Chef derrière Le Lazare
         </h3>
